386f83cfed52afafd2bfa61c48c173a9a9fd6bfc
Biró Szabolcs Benjámin <70518815+BiroSzabolcsBenjamin@users.noreply.github.com>
2025-10-13 09:25:16 +0200

    angular

---
+++ dom/01-starting-project-main/01-starting-project-main/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
+
+DUMMY_USERS
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule,HeaderComponent, UserComponent, TasksComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+    users = DUMMY_USERS;
+    selectedUserId?:  string;
+
+    get selectedUser() {
+      return this.users.find((user) => user.id === this.selectedUserId);
+    }
+
+    onSelectUser(id: string) {
+      this.selectedUserId = id;
+    }
+}
