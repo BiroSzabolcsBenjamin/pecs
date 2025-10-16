3823194763111ab55c853eaf3183fc21a58bf747
Biró Szabolcs Benjámin <70518815+BiroSzabolcsBenjamin@users.noreply.github.com>
2025-10-16 14:18:08 +0200

    igen

--- gergo/loginFX/src/loginfx/loginFXController.java
+++ gergo/loginFX/src/loginfx/loginFXController.java
@@ -5,6 +5,7 @@
 package loginfx;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -21,7 +22,7 @@ import javafx.scene.control.TextField;
  * @author szabi
  */
 public class loginFXController implements Initializable {
-    
+
     @FXML
     private Label outputLabel;
     @FXML
@@ -34,16 +35,39 @@ public class loginFXController implements Initializable {
     private DatePicker birthDate;
     @FXML
     private CheckBox checkbox;
-    
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        outputLabel.setText("Hello World!");
+    private void handleRegister(ActionEvent event) {
+
+        String name = registerName.getText();
+        String password = registerPassword.getText();
+        LocalDate date = birthDate.getValue();
+        boolean adomentes = checkbox.isSelected();
+
+        if (name.isEmpty() && password.isEmpty() && date == null) {
+            outputLabel.setText("Kérlek add meg az összes adatot!");
+        } else if (name.isEmpty()) {
+            outputLabel.setText("Kérlek adj meg egy nevet");
+        } else if (password.isEmpty()) {
+            outputLabel.setText("Kérlek adj meg egy jelszót!");
+        } else if (date == null) {
+            outputLabel.setText("Kérlek adj meg egy születési dátumot");
+        } else if (name.isEmpty() && password.isEmpty()) {
+            outputLabel.setText("Kérlek adj meg egy nevet és jelszót");
+        } else if (name.isEmpty() && date == null) {
+            outputLabel.setText("Kérlek adj meg egy nevet és születési dátumot");
+        } else if (password.isEmpty() && date == null) {
+            outputLabel.setText("Kérlek adj meg egy jelaszót egy és születési dátumot");
+        } else {
+            outputLabel.setText("Sikeresen regisztrált!\n" + name + "\n" + password + "\n" + date + "\n" + adomentes);
+
+        }
+
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
