9f7fc3396c45b53b27ac7ef6097c96ac89975181
Biró Szabolcs Benjámin <70518815+BiroSzabolcsBenjamin@users.noreply.github.com>
2026-01-13 20:45:31 +0100

    Angular

--- levi/11.17/resource/cat.js
+++
@@ -1,6 +0,0 @@
-/*
-document.addEventListener('DOMContentLoaded', () => {
-    let gender = document.querySelector('#gender').value;
-    fetch(`../controller/catController.php?gender=${gender}`)
-});
-*/
