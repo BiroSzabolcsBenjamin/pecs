c8e7c8737520f2880be03dc80e9febc10c9079e2
Biró Szabolcs Benjámin <70518815+BiroSzabolcsBenjamin@users.noreply.github.com>
2026-02-03 14:06:37 +0100

    a

--- udemy-forms/src/app/auth/login/login.component.ts
+++ udemy-forms/src/app/auth/login/login.component.ts
@@ -1,26 +1,59 @@
-import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { Component} from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { of } from 'rxjs';
+
+function mustContainQuestionMark(control: AbstractControl){
+  if(control.value.includes('?')){
+    return null;
+  }
+  return{ doesNotContainQuestionMark: true };
+}
+
+function emailIsUnique(control: AbstractControl){
+  if(control.value !== "test@example.com"){
+    return of(null);
+  }
+  return of({ notUnique: true });
+}
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  onSubmit(formData: NgForm){
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [ Validators.email, Validators.required],
+      asyncValidators: [emailIsUnique]
+    }),
+    password: new FormControl('', {
+      validators: [ Validators.required, Validators.minLength(6), mustContainQuestionMark]
 
-    if(formData.form.invalid){
-      return;
-    }
+    })
+  });
 
-    const enteredEmail = formData.form.value.email;
-    const enteredPassword = formData.form.value.password;
+  get emailIsInvalid(){
+    return (
+      this.form.controls.email.touched &&
+      this.form.controls.email.dirty &&
+      this.form.controls.email.invalid
+    );
+  }
+  get passwordIsInvalid(){
+    return (
+      this.form.controls.password.touched &&
+      this.form.controls.password.dirty &&
+      this.form.controls.password.invalid
+    );
+  }
 
-    console.log(formData.form);
-    console.log(enteredEmail, enteredPassword);
-
-
+  onSubmit(){
+    console.log(this.form)
+    const enteredEmial = this.form.value.email;
+    const enteredPassword = this.form.value.password;
+    console.log(enteredEmial,enteredPassword)
   }
 }
